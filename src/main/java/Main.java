d694b84b94428cc2da2934cc5b1ba59955f593e1
Katsching <kai.chen@gmx.de>
2020-05-28 17:50:23 +0200

    * Added calendar functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import calendarFunctions.CalendarQuickstart;
 import weatherForecast.WeatherService;
 
 public class Main {
@@ -5,5 +12,23 @@ public class Main {
     public static void main(String[] args) {
     	WeatherService weatherServices = new WeatherService();
     	weatherServices.getWeatherFromCity("Stuttgart", "DE");
+    	
+    	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    	Date currentDateObj = new Date();
+       	
+    	String dateString = df.format(currentDateObj);
+//    	createNewEvent("Test", "Stuttgart", "Wetterinfo", dateString);
+    	try {
+			CalendarQuickstart.updateEvent("UpdatedTest6", "Stuggi", "Wetterinfo", dateString);
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
+    
+    
+
 }
---
+++ src/main/java/calendarFunctions/CalendarQuickstart.java
@@ -0,0 +1,130 @@
+package calendarFunctions;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class CalendarQuickstart {
+    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_EVENTS);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    
+    public static void createNewEvent(String summary, String location, String description, String date) throws GeneralSecurityException, IOException {
+    	Calendar service = initiateTransport();
+        
+    	Event event = new Event()
+        		    .setSummary(summary)
+        		    .setLocation(location)
+        		    .setDescription(description);
+         String eventID = date.replace("-", "");
+             
+         DateTime startDateTime = new DateTime(date);
+         EventDateTime allDayTime = new EventDateTime()
+             .setDate(startDateTime)
+             .setTimeZone("Europe/Berlin");
+         event.setStart(allDayTime);
+         event.setEnd(allDayTime);
+         
+         event.setId(eventID);
+
+         
+         String calendarId = "primary";
+         event = service.events().insert(calendarId, event).execute();
+         
+         System.out.printf("Event created: %s\n", event.getHtmlLink());
+    }
+    
+    public static void updateEvent(String summary, String location, String description, String date) throws GeneralSecurityException, IOException {
+         Calendar service = initiateTransport();
+
+         Event updatedEvent = new Event()
+     		    .setSummary(summary)
+     		    .setLocation(location)
+     		    .setDescription(description);
+      
+      DateTime startDateTime = new DateTime(date);
+      EventDateTime allDayTime = new EventDateTime()
+          .setDate(startDateTime)
+          .setTimeZone("Europe/Berlin");
+      updatedEvent.setStart(allDayTime);
+      updatedEvent.setEnd(allDayTime);
+      
+      String calendarId = "primary";
+      String eventId = date.replace("-", "");
+     
+      
+      updatedEvent = service.events().update(calendarId, eventId, updatedEvent).execute();
+    	
+    }
+    
+    private static Calendar initiateTransport() throws GeneralSecurityException, IOException {
+    	final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        return service;
+    }
+    
+    
+    private void createEventTitle() {
+    	// format example: Rainy - 18°C
+    }
+    
+    private void createWeatherDescription() {
+    	//TODO
+    	// example: UV value
+    }
+    
+}
